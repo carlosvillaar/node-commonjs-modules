5bae825f4abc98037b75e9dc05a31505f8a92f4f
carlosvillaar <carlosvillar004@gmail.com>
2024-12-16 14:26:27 +0100

    feat: add second file with hobbies function

---
+++ hobbies.js
@@ -0,0 +1,9 @@
+function hobbies (hobbyOne, hobbyeTwo, hobbieThree){
+  const hobbiesArray = [];
+  hobbiesArray.push(hobbyOne, hobbyeTwo, hobbieThree)
+  return {
+    hobbies: hobbiesArray
+  }
+}
+
+module.exports = hobbies;
